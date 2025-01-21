08e3a09f8d5a6baeae6cb452af5a201de0f46e3c
David Konsumer <konsumer@jetboystudio.com>
2025-01-20 19:34:51 -0800

    felshing things out

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#define CVECTOR_LOGARITHMIC_GROWTH
+#include "cvector.h"
+
+#define PNTR_IMPLEMENTATION
+#define PNTR_ENABLE_DEFAULT_FONT
+#include "pntr.h"
+
+#define PNTR_BRUSH_IMPLEMENTATION
+#include "pntr_brush.h"
+
+#include "null0_api_drawing.h"
+
+int main() {
+    return 0;
+}
